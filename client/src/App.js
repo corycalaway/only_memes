d8811bb00b448da6068dfcfc44aeefa4cb447c37
Cory Calaway <cory.c.calaway@gmail.com>
2021-04-20 16:00:06 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+
+
+function App() {
+  console.log(Counter)
+  return (
+    <div className="App">
+     <h1>Only Memes</h1>
+    </div>
+  );
+}
+
+export default App;
