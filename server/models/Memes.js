6ee57ea83b8ab731c07bf7fd9c559fbed71e8829
Paul Chang <paulchang959@gmail.com>
2021-04-21 12:31:19 -0700

    preset up of Models

---
+++ server/models/Memes.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const memeSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  rarity: {
+    type: String,
+    trim: true,
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+});
+
+const Memes = mongoose.model("Memes", memeSchema);
+module.exports = Memes;
--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,53 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const memeSchema = require("./Memes");
+
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        trim: ture
-    }
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 4,
+    },
+    credit: {
+      type: Number,
+    },
+    memes: [memeSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
