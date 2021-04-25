da9533e8b320891357035c2d324b75503d4f69aa
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-04-25 16:32:11 -0600

    displaying memes in profile should kind of work I think?

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { QUERY_USER } from "../utils/queries";
+import { Container, Row, Col, Jumbotron, Card } from "react-bootstrap";
+import { useQuery } from "@apollo/client";
+
+function Profile() {
+  const { data } = useQuery(QUERY_USER);
+  let user;
+
+  if (data) {
+    user = data.users[0];
+    console.log(user);
+  }
+
+  return (
+    <>
+      {user ? (
+        <>
+          {user.memes.map((meme) => (
+            <Card>
+              <Card.Img variant="top" src={meme.image} />
+              <Card.Body>
+                <Card.Title>{meme.title}</Card.Title>
+                <Card.Text>Rarity: {meme.rarity}</Card.Text>
+                <Card.Text>Category: {meme.category}</Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </>
+      ) : null}
+    </>
+  );
+}
+
+export default Profile;
