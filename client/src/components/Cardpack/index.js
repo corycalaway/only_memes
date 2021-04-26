b7579db1cdae6e837cb48a3eb334cfadfd14e68c
Cory Calaway <cory.c.calaway@gmail.com>
2021-04-26 12:23:58 -0600

    first working error for packs

--- client/src/components/Cardpack/index.js
+++ client/src/components/Cardpack/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { Form, Button, Container, Row, Jumbotron, Card } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/react-hooks";
@@ -16,63 +16,78 @@ const Cardpack = () => {
     // const user = userData?.me || userData?.user || {};
 
     const { loading, data: userData } = useQuery(QUERY_ME)
- 
+    const [ enoughCredits, updateCredits] = useState({})
+
 
     const handleFormSubmit = async () => {
-        console.log(userData)
+        console.log(userData.me.credit)
         const token = Auth.loggedIn() ? Auth.getToken() : null;
+        let tempCredit = userData.me.credit
 
-        if (!token) {
-            console.log("no token");
+
+        if (userData.me.credit <= 0) {
+               console.log(tempCredit)
+          
+            updateCredits(tempCredit)
+
+              
+           
+           console.log(enoughCredits)
         } else {
-            var { memes } = data;
-            console.log(memes);
-            let highPrize = [];
-            let lowPrize = [];
-            let packMemes = [];
+            
+           
+            if (!token) {
+                console.log("no token");
+            } else {
+                var { memes } = data;
+                console.log(memes);
+                let highPrize = [];
+                let lowPrize = [];
+                let packMemes = [];
 
-            for (let i = 0; i < memes.length; i++) {
-                if (memes[i].rarity === "Legendary" || memes[i].rarity === "Epic") {
-                    highPrize.push(memes[i]);
-                } else {
-                    lowPrize.push(memes[i]);
+                for (let i = 0; i < memes.length; i++) {
+                    if (memes[i].rarity === "Legendary" || memes[i].rarity === "Epic") {
+                        highPrize.push(memes[i]);
+                    } else {
+                        lowPrize.push(memes[i]);
+                    }
                 }
-            }
-            console.log(highPrize);
-            console.log(lowPrize);
+                console.log(highPrize);
+                console.log(lowPrize);
 
-            let highSelect = Math.floor(Math.random() * highPrize.length + 1) - 1;
-            let lowSelect = Math.floor(Math.random() * highPrize.length + 1) - 1;
-            let lowSelect2 = Math.floor(Math.random() * highPrize.length + 1) - 1;
+                let highSelect = Math.floor(Math.random() * highPrize.length + 1) - 1;
+                let lowSelect = Math.floor(Math.random() * highPrize.length + 1) - 1;
+                let lowSelect2 = Math.floor(Math.random() * highPrize.length + 1) - 1;
 
-            while (lowSelect === lowSelect2) {
-                lowSelect2 = Math.floor(Math.random() * highPrize.length + 1) - 1;
-            }
-
-            let addOne = highPrize[highSelect]._id;
-            // let addTwo = lowPrize[lowSelect];
-            // let addThree = lowPrize[lowSelect2];
-            packMemes.push(highPrize[highSelect]._id);
-            packMemes.push(lowPrize[lowSelect]._id);
-            packMemes.push(lowPrize[lowSelect2]._id);
-
-            console.log(packMemes);
-            for (let i = 0; i < packMemes.length; i++) {
-                try {
-                    console.log(addOne);
-
-                    const { data } = await addCollection({
-                        variables: {
-                            memeId: packMemes[i],
-                        },
-                    });
-                    console.log(data);
-                    Auth.loggedIn(token);
-                } catch (e) {
-                    console.error(e);
+                while (lowSelect === lowSelect2) {
+                    lowSelect2 = Math.floor(Math.random() * highPrize.length + 1) - 1;
                 }
+
+                let addOne = highPrize[highSelect]._id;
+                // let addTwo = lowPrize[lowSelect];
+                // let addThree = lowPrize[lowSelect2];
+                packMemes.push(highPrize[highSelect]._id);
+                packMemes.push(lowPrize[lowSelect]._id);
+                packMemes.push(lowPrize[lowSelect2]._id);
+
+                console.log(packMemes);
+                for (let i = 0; i < packMemes.length; i++) {
+                    try {
+                        console.log(addOne);
+
+                        const { data } = await addCollection({
+                            variables: {
+                                memeId: packMemes[i],
+                            },
+                        });
+                        console.log(data);
+                        Auth.loggedIn(token);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+                // add collection to user
             }
-            // add collection to user
         }
     };
 
@@ -92,12 +107,12 @@ const Cardpack = () => {
                         <h3>Purchase a Pack!</h3>
                     </Row>
                     <Row className="justify-content-center">
-                    <Card style={{ width: "20rem" }}>
-                        <img
-                            src={cardpackImage}
-                            alt=""
-                        />
-                    </Card>
+                        <Card style={{ width: "20rem" }}>
+                            <img
+                                src={cardpackImage}
+                                alt=""
+                            />
+                        </Card>
                     </Row>
                     <Row className="justify-content-center">
 
@@ -110,8 +125,17 @@ const Cardpack = () => {
                                     </Button>
                             </div>
                         </Row>
-
                     </Row>
+
+                    {enoughCredits <= 0 && 
+                        <Row className="justify-content-center">
+                            <div>
+                                <h3>You do not have enough Credits!</h3>
+
+                            </div>
+                        </Row> 
+                    }
+
 
                     <Row className="justify-content-center"></Row>
                 </Card>
