a19cfdccbcb99ad0088363643cf8248f5374bc0f
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-04-26 20:50:11 -0600

    changed nav a tad

--- client/src/components/NavHeader/index.js
+++ client/src/components/NavHeader/index.js
@@ -11,6 +11,10 @@ function NavHeader() {
     if (Auth.loggedIn()) {
       return (
         <>
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link>My Memes</Nav.Link>
+          <Nav.Link href="/community">Community</Nav.Link>
+          <Nav.Link href="/aboutus">About Us</Nav.Link>
           <Nav.Link href="/" onClick={() => Auth.logout()}>
             Logout
           </Nav.Link>
@@ -29,35 +33,12 @@ function NavHeader() {
     }
   };
 
-  let Profile = () => {
-    if (Auth.loggedIn()) {
-      return (
-        <>
-          <Nav.Link>My Memes</Nav.Link>
-        </>
-      );
-    } else {
-      return;
-    }
-  };
-
   return (
     <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
       <Navbar.Brand href="#Home">OnlyMemes!</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          {tabs.map((tab) => (
-            <Nav.Link
-              key={tab}
-              href={"#" + tab.toLowerCase()}
-              //onClick={() => props.handlePageChange(tab)}
-            >
-              {tab}
-            </Nav.Link>
-          ))}
-          {Profile()}
-        </Nav>
+        <Nav className="mr-auto"></Nav>
         <NavDropdown.Divider />
         <Nav>{Logged()}</Nav>
       </Navbar.Collapse>
