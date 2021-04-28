14ed34d4b9080ef222b2c32f8a04bce355354652
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-04-27 19:22:48 -0600

    get memes doesn't show up if not logged in

--- client/src/components/Cardpack/index.js
+++ client/src/components/Cardpack/index.js
@@ -11,151 +11,151 @@ import { addNewMemes, cardReset } from "../../utils/actions/";
 import OpenPack from "./OpenPack";
 
 const Cardpack = () => {
-    const { data } = useQuery(QUERY_MEMES);
-    const dispatch = useDispatch();
+  const { data } = useQuery(QUERY_MEMES);
+  const dispatch = useDispatch();
 
-    const memedisplay = useSelector((state) => state.memeDisplayReducer);
+  const memedisplay = useSelector((state) => state.memeDisplayReducer);
 
-    // const { userData } = useQuery(QUERY_USER)
-    // const { username: userParam } = useParams();
+  // const { userData } = useQuery(QUERY_USER)
+  // const { username: userParam } = useParams();
 
-    const [addCollection, { error }] = useMutation(ADD_COLLECTION);
-    // const user = userData?.me || userData?.user || {};
+  const [addCollection, { error }] = useMutation(ADD_COLLECTION);
+  // const user = userData?.me || userData?.user || {};
 
-    const { loading, data: userData } = useQuery(QUERY_ME);
-    const [enoughCredits, updateCredits] = useState({});
+  const { loading, data: userData } = useQuery(QUERY_ME);
+  const [enoughCredits, updateCredits] = useState({});
 
-    const handleFormSubmit = async () => {
-        let tempCredit;
+  const handleFormSubmit = async () => {
+    let tempCredit;
 
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-        if (Auth.loggedIn()) {
-            tempCredit = userData.me.credit;
-        } else {
-            tempCredit = "notloggedin";
-            updateCredits(tempCredit);
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (Auth.loggedIn()) {
+      tempCredit = userData.me.credit;
+    } else {
+      tempCredit = "notloggedin";
+      updateCredits(tempCredit);
+    }
+
+    if (tempCredit <= 0) {
+      updateCredits(tempCredit);
+    } else {
+      if (!token) {
+      } else {
+        var { memes } = data;
+        let highPrize = [];
+        let lowPrize = [];
+        let packMemes = [];
+        let fullPack = [];
+        console.log(memes.length);
+        for (let i = 0; i < memes.length; i++) {
+          if (memes[i].rarity === "Legendary" || memes[i].rarity === "Epic") {
+            highPrize.push(memes[i]);
+          } else {
+            lowPrize.push(memes[i]);
+          }
         }
 
-        if (tempCredit <= 0) {
-            updateCredits(tempCredit);
-        } else {
-            if (!token) {
-            } else {
-                var { memes } = data;
-                let highPrize = [];
-                let lowPrize = [];
-                let packMemes = [];
-                let fullPack = [];
-                console.log(memes.length);
-                for (let i = 0; i < memes.length; i++) {
-                    if (memes[i].rarity === "Legendary" || memes[i].rarity === "Epic") {
-                        highPrize.push(memes[i]);
-                    } else {
-                        lowPrize.push(memes[i]);
-                    }
-                }
+        let highSelect = Math.floor(Math.random() * highPrize.length + 1) - 1;
+        let lowSelect = Math.floor(Math.random() * lowPrize.length + 1) - 1;
+        let lowSelect2 = Math.floor(Math.random() * lowPrize.length + 1) - 1;
 
-                let highSelect = Math.floor(Math.random() * highPrize.length + 1) - 1;
-                let lowSelect = Math.floor(Math.random() * lowPrize.length + 1) - 1;
-                let lowSelect2 = Math.floor(Math.random() * lowPrize.length + 1) - 1;
+        while (lowSelect === lowSelect2) {
+          lowSelect2 = Math.floor(Math.random() * lowPrize.length + 1) - 1;
+        }
 
-                while (lowSelect === lowSelect2) {
-                    lowSelect2 = Math.floor(Math.random() * lowPrize.length + 1) - 1;
-                }
+        let addOne = highPrize[highSelect]._id;
 
-                let addOne = highPrize[highSelect]._id;
+        packMemes.push(highPrize[highSelect]._id);
+        packMemes.push(lowPrize[lowSelect]._id);
+        packMemes.push(lowPrize[lowSelect2]._id);
 
-                packMemes.push(highPrize[highSelect]._id);
-                packMemes.push(lowPrize[lowSelect]._id);
-                packMemes.push(lowPrize[lowSelect2]._id);
+        fullPack.push(highPrize[highSelect]);
+        fullPack.push(lowPrize[lowSelect]);
+        fullPack.push(lowPrize[lowSelect2]);
+        console.log(fullPack);
+        let result = dispatch(addNewMemes(fullPack));
 
-                fullPack.push(highPrize[highSelect]);
-                fullPack.push(lowPrize[lowSelect]);
-                fullPack.push(lowPrize[lowSelect2]);
-                console.log(fullPack);
-                let result = dispatch(addNewMemes(fullPack));
-
-                if (packMemes) {
-                    for (let i = 0; i < packMemes.length; i++) {
-                        try {
-                            const { data } = await addCollection({
-                                variables: {
-                                    memeId: packMemes[i],
-                                },
-                            });
-                            Auth.loggedIn(token);
-                        } catch (e) {
-                            console.error(e);
-                        }
-                    }
-                }
-                // add collection to user
+        if (packMemes) {
+          for (let i = 0; i < packMemes.length; i++) {
+            try {
+              const { data } = await addCollection({
+                variables: {
+                  memeId: packMemes[i],
+                },
+              });
+              Auth.loggedIn(token);
+            } catch (e) {
+              console.error(e);
             }
+          }
         }
-    };
+        // add collection to user
+      }
+    }
+  };
 
-    // if (!user?.username) {
-    //     return (
-    //         <h4>
-    //             You need to be logged in to see this page. Use the navigation links above to sign up or log in!
-    //         </h4>
-    //     );
-    // }
+  // if (!user?.username) {
+  //     return (
+  //         <h4>
+  //             You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+  //         </h4>
+  //     );
+  // }
 
-    return (
-        <Container fluid>
+  return (
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Card style={{ width: "30rem" }}>
+          <Card.Body>
             <Row className="justify-content-center">
-                <Card style={{ width: "30rem" }}>
-                    <Card.Body>
-                        <Row className="justify-content-center">
-                            <h3>Purchase a Pack!</h3>
-                        </Row>
-                        <Row className="justify-content-center">
-                            <Card style={{ width: "20rem" }}>
-                                <img src={cardpackImage} alt="" />
-                            </Card>
-
-                            
-                        </Row>
-
-                        <Row className="justify-content-center">
-                            {" "}
-                            <Button
-                                style={buttonStyle}
-                                variant="dark"
-                                type="submit"
-                                onClick={handleFormSubmit}
-                            >
-                                Get Memes!
-              </Button>
-                        </Row>
-
-                        {enoughCredits <= 0 && (
-                            <Row className="justify-content-center">
-                                <div>
-                                    <h3>You do not have enough Credits!</h3>
-                                </div>
-                            </Row>
-                        )}
-
-                        {enoughCredits === "notloggedin" && (
-                            <Row className="justify-content-center">
-                                <div>
-                                    <h3>You must be logged in to purchase!</h3>
-                                </div>
-                            </Row>
-                        )}
-
-                        <Row className="justify-content-center"></Row>
-                    </Card.Body>
-                </Card>
+              <h3>Purchase a Pack!</h3>
             </Row>
-            <OpenPack />
-        </Container>
-    );
+            <Row className="justify-content-center">
+              <Card style={{ width: "20rem" }}>
+                <img src={cardpackImage} alt="" />
+              </Card>
+            </Row>
+
+            {Auth.loggedIn() ? (
+              <Row className="justify-content-center">
+                {" "}
+                <Button
+                  style={buttonStyle}
+                  variant="dark"
+                  type="submit"
+                  onClick={handleFormSubmit}
+                >
+                  Get Memes!
+                </Button>
+              </Row>
+            ) : null}
+
+            {enoughCredits <= 0 && (
+              <Row className="justify-content-center">
+                <div>
+                  <h3>You do not have enough Credits!</h3>
+                </div>
+              </Row>
+            )}
+
+            {enoughCredits === "notloggedin" && (
+              <Row className="justify-content-center">
+                <div>
+                  <h3>You must be logged in to purchase!</h3>
+                </div>
+              </Row>
+            )}
+
+            <Row className="justify-content-center"></Row>
+          </Card.Body>
+        </Card>
+      </Row>
+      <OpenPack />
+    </Container>
+  );
 };
 
 const buttonStyle = {
-    background: "red",
+  background: "red",
 };
 export default Cardpack;
